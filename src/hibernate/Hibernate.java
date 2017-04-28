7226714be23b39584839a89b595e0b0b6d8e6598
jesus071296 <chema1996_@hotmail.com>
2017-04-28 08:33:43 -0500

    first commit

---
+++ src/hibernate/Hibernate.java
@@ -0,0 +1,16 @@
+
+
+ 
+package Hibernate;
+
+
+public class Hibernate {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
